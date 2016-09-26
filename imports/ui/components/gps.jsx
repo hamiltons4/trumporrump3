ee0ea3e938a8d416d2061dab357f1eb75dfa28c3
Hamilton Smith <smithhamilton4@gmail.com>
2016-09-26 06:02:19 -0400

    added amazon links. adjusted target and inline style for React

--- imports/ui/components/gps.jsx
+++ imports/ui/components/gps.jsx
@@ -8,8 +8,20 @@ export default class Gps extends Component {
 		return (
 			<div className="gps">
 			   	<h2><span className="label">Having a hard time choosing? Use</span></h2>
-				<div><button className="btn btn-primary" id="innergps" type="submit"><a href="#">Your Inner GPS</a></button></div>		
-			</div>
+				{/*<div><button className="btn btn-primary" id="innergps" type="submit"><a href="#">Your Inner GPS</a></button></div>*/}
+				<div>
+					<div>
+					{/*<button className="btn btn-primary" id="innergps" type="submit">*/}
+						<a href="https://www.amazon.com/gp/product/1608684121/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1608684121&linkCode=
+						as2&tag=trumporrump-20&linkId=01352b657bb7d5b8613591f4ab95f662" target="_blank"><img border="0" src=
+						"//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1608684121&ServiceVersion=20070822&ID=
+						AsinImage&WS=1&Format=_SL160_&tag=trumporrump-20" /></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=trumporrump-20&l=am2&o=
+						1&a=1608684121" width="1" height="1" border="0" alt="" style={{border:'none !important', margin:'0px !important'}} />
+					{/*</button>*/}
+					</div>
+					
+				</div>
+			</div>	
 		)
 	}
 }
--- imports/ui/components/principles.jsx
+++ imports/ui/components/principles.jsx
@@ -8,9 +8,23 @@ export default class Principles extends Component {
 		return (
 			<div className="princip">
 			   	<h2><span className="label">Election stressing you out? Try</span></h2>
-				<div><button className="btn btn-primary" id="yoga" type="submit"><a href="#">Mind Yoga</a></button></div>
+				<div>
+				{/*<button className="btn btn-primary" id="yoga" type="submit"><a href="#">Mind Yoga</a></button>*/}
+				<a href="https://www.amazon.com/gp/product/0692709452/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0692709452&linkCode=
+				as2&tag=trumporrump-20&linkId=6a4c5a75b4ebdc236b10a692beb347e7" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=
+				UTF8&MarketPlace=US&ASIN=0692709452&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=trumporrump-20" /></a>
+				<img src="//ir-na.amazon-adsystem.com/e/ir?t=trumporrump-20&l=am2&o=1&a=0692709452" width="1" height="1" border="0" alt="" 
+				style={{border:'none !important', margin:'0px !important'}} />
+				</div>
 				<div><h2 id="orprinciples"><span className="label" >Or</span></h2></div>
-				<div><button className="btn btn-primary" id="alchemy" type="submit"><a href="#">Alchemy</a></button></div>
+				<div>
+				{/*<button className="btn btn-primary" id="alchemy" type="submit"><a href="#">Alchemy</a></button></div>*/}
+				<a href="https://www.amazon.com/gp/product/0981376304/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0981376304&linkCode=
+				as2&tag=trumporrump-20&linkId=c15111c356005c73742d4ee556b5595a" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=
+				UTF8&MarketPlace=US&ASIN=0981376304&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=trumporrump-20" /></a>
+				<img src="//ir-na.amazon-adsystem.com/e/ir?t=trumporrump-20&l=am2&o=1&a=0981376304" width="1" height="1" border="0" alt="" 
+				style={{border:'none !important', margin:'0px !important'}} />
+				</div>
 			</div>
 		)
 	}
